cc13a04e0bf05e135f7665d36b5c5f31e39c7f66
IamBemooooo <nguyendinhthe405@gmail.com>
2024-12-03 17:06:47 +0700

    change cache type

--- Utilities/Contants/CacheType.cs
+++ Utilities/Contants/CacheType.cs
@@ -4,26 +4,26 @@
     {
         public const int REMOVE = 0;
         public const int REMOVE_AND_RE_LOAD = 1;
-        public const string USER_LIST = "USER_LIST_";// Lấy ra danh sách User
-        public const string ARTICLE_CATEGORY_ID = "ARTICLE_CATEGORY_";
-        public const string CATEGORY_NEWS = "CATEGORY_NEWS_";
-        public const string CATEGORY_SEARCH = "CATEGORY_SEARCH_";
-        public const string CATEGORY_TAG = "CATEGORY_TAG_";
-        public const string ARTICLE_ID = "ARTICLE_";
-        public const string ARTICLE_MOST_VIEWED = "ARTICLE_MOST_VIEWED";
-        public const string ARTICLE_CATEGORY_MENU = "ARTICLE_CATEGORY_MENU_";
-        public const string ARTICLE_FOOTER_MENU = "ARTICLE_FOOTER_MENU";
+        public const string USER_LIST = "USER_LIST_OCLOCK_";// Lấy ra danh sách User
+        public const string ARTICLE_CATEGORY_ID = "ARTICLE_CATEGORY_OCLOCK_";
+        public const string CATEGORY_NEWS = "CATEGORY_NEWS_OCLOCK_";
+        public const string CATEGORY_SEARCH = "CATEGORY_SEARCH_OCLOCK_";
+        public const string CATEGORY_TAG = "CATEGORY_TAG_OCLOCK_";
+        public const string ARTICLE_ID = "ARTICLE_OCLOCK_";
+        public const string ARTICLE_MOST_VIEWED = "ARTICLE_MOST_VIEWEDOCLOCK_";
+        public const string ARTICLE_CATEGORY_MENU = "ARTICLE_CATEGORY_MENU_OCLOCK_";
+        public const string ARTICLE_FOOTER_MENU = "ARTICLE_FOOTER_MENUOCLOCK_";
 
-        public const string PRODUCT_LISTING = "PRODUCT_LISTING_";
-        public const string PRODUCT_DETAIL = "PRODUCT_DETAIL_";
-        public const string ORDER_DETAIL_FE = "ORDER_DETAIL_FE_";
+        public const string PRODUCT_LISTING = "PRODUCT_LISTING_OCLOCK_";
+        public const string PRODUCT_DETAIL = "PRODUCT_DETAIL_OCLOCK_";
+        public const string ORDER_DETAIL_FE = "ORDER_DETAIL_FE_OCLOCK_";
 
-        public const string ADDRESS_CLIENT = "ADDRESS_CLIENT_";
-        public const string TOP_STORY = "TOP_STORY";
+        public const string ADDRESS_CLIENT = "ADDRESS_CLIENT_OCLOCK_";
+        public const string TOP_STORY = "TOP_STORYOCLOCK_";
 
-        public const string PRODUCT_BRAND = "PRODUCT_BRAND";
-        public const string PRODUCT_BY_BRAND = "PRODUCT_BY_BRAND_";
-        public const string PRODUCT_BY_PRICERANGE = "PRODUCT_BY_PRICERANGE_";
+        public const string PRODUCT_BRAND = "PRODUCT_BRANDOCLOCK_";
+        public const string PRODUCT_BY_BRAND = "PRODUCT_BY_BRAND_OCLOCK_";
+        public const string PRODUCT_BY_PRICERANGE = "PRODUCT_BY_PRICERANGE_OCLOCK_";
 
     }
 }
