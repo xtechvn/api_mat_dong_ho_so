bb43a947f00775333feae06fa7716c05b0b37958
IamBemooooo <nguyendinhthe405@gmail.com>
2024-12-04 08:51:12 +0700

    config

--- Digital_oClock_Api/Models/GroupProduct/GroupProductESModel.cs
+++ Digital_oClock_Api/Models/GroupProduct/GroupProductESModel.cs
@@ -1,32 +1,35 @@
-﻿namespace HuloToys_Service.Models.ElasticSearch
+﻿using Nest;
+using System.Runtime.InteropServices;
+
+namespace HuloToys_Service.Models.ElasticSearch
 {
     public partial class GroupProductModel
     {
+        [PropertyName("id")]
         public int id { get; set; }
-
+        [PropertyName("ParentId")]
         public int parentid { get; set; }
-
+        [PropertyName("PositionId")]
         public int? positionid { get; set; }
-         
+        [PropertyName("Name")]
         public string name { get; set; } = null!;
-
+        [PropertyName("ImagePath")]
         public string? imagepath { get; set; }
-
+        [PropertyName("OrderNo")]
         public int? orderno { get; set; }
-
+        [PropertyName("Path")]
         public string? path { get; set; }
-
+        [PropertyName("Status")]
         public int? status { get; set; }
-
+        [PropertyName("CreatedOn")]
         public DateTime? createdon { get; set; }
-
+        [PropertyName("ModifiedOn")]
         public DateTime? modifiedon { get; set; }
-
-
+        [PropertyName("Description")]
         public string? description { get; set; }
-
+        [PropertyName("IsShowHeader")]
         public bool isshowheader { get; set; }
-
+        [PropertyName("IsShowFooter")]
         public bool isshowfooter { get; set; }
 
         public List<GroupProductModel> group_product_child { get; set; }
